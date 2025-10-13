4a78274ef60f3a9a64716c6e1bb5f714a4973f01
LegitMartin <martin.kubicka2004@gmail.com>
2025-10-13 18:54:05 +0200

    exercise.py is now properly functional

--- Book Analysis/exercise.py
+++ Book Analysis/exercise.py
@@ -1,41 +1,89 @@
 import re
 
-# Finds how many word occurrences there are in the book
+# Change the book name to scan through a different book
+# It should be a path to the file you wish to open and scan.
+book_name = "miracle_in_the_andes.txt"
+
 def find_word(word, book):
-    print(word)
-    pattern = re.compile(word + "[^a-zA-Z]")
+    """
+    Counts how many times the word appears in the text.
+
+    :param word: String, the word to look for
+    :param book: String containing the full text read from a book file.
+    :return: Number of matches, or a message if none are found.
+    """
+    pattern = re.compile(word.lower() + "[^a-zA-Z]")
     findings = re.findall(pattern, book.lower())
-    print(findings)
+
+    # Prints the list of all the occurrences
+    #print(findings)
+
     matches = 0
     for i in findings:
-        if word in i:
+        if word.lower() in i:
             matches += 1
     if matches == 0:
         return "The book does not contain the word \"" + word + "\""
 
-    with open("output.txt", "w", encoding="utf-8") as file:
-        for i in range(len(findings)):
-            book = file.write(findings[i] + "\n")
+    # Debug code for testing
+    # with open("output.txt", "w", encoding="utf-8") as file:
+    #    for i in range(len(findings)):
+    #        book = file.write(findings[i] + "\n")
+
     return matches
 
 
-with open("miracle_in_the_andes.txt", "r", encoding="utf-8") as file:
-    book = file.read()
+def chapter_titles(book):
+    """
+    Finds possible chapter titles in the text.
+    Requires proper formating of chapters in the book to work properly
+
+    :param book: String containing the full text read from a book file.
+    :return: List of chapter title candidates.
+    """
+    pattern = re.compile("[a-zA-Z ,]+\n\n")
+    findings = re.findall(pattern, book)
+    findings = [item.strip("\n\n") for item in findings]
+    return findings
 
 
-# Paragraphs with love
-# pattern = re.compile("[^\n]*[^a-zA-Z]+love[^\n]*")
-# findings = re.findall(pattern, book)
+def find_sentences(word, book):
+    """
+    Returns all sentences containing the word user enters
 
-# Titles of chapters
-# pattern = re.compile("[a-zA-Z ,]+\n\n")
-# findings = re.findall(pattern, book)
-# findings = [item.strip("\n\n") for item in findings]
+    :param word: String, the word to look for
+    :param book: String containing the full text read from a book file.
+    :return: List of sentences with 'love'.
+    """
+    pattern = re.compile("[A-Z]{1}[^.]*[^a-zA-Z]" + word.lower() + "[^a-zA-Z]+[^.]*")
+    findings = re.findall(pattern, book)
+    return findings
 
-# Occurence of any word
-word = input("Input a word: ")
-print(find_word(word, book))
 
+# Initial prompt for the user to choose which function he would like to use
+function_name = input("Which function would you like to use?" +
+                      "\n1: Find the occurrences of a certain word in the book." +
+                      "\n2: Gets the titles of chapters" +
+                      "\n3: Finds the sentences which contain the word \"love\"" +
+                      "\nType in a number: ")
+
+# Opens the book file
+with open(book_name, "r", encoding="utf-8") as file:
+    book_text = file.read()
+
+# Calls the proper function
+if function_name == '1':
+    word = input("Input a word: ")
+    print("Number of matches found: " + str(find_word(word, book_text)))
+elif function_name == '2':
+    print(chapter_titles(book_text))
+elif function_name == '3':
+    word = input("Input a word: ")
+    print(find_sentences(word, book_text))
+else:
+    print("Invalid Input")
+
+# Debug code for testing
 """
 print(findings)
 
@@ -43,4 +91,3 @@ with open("output.txt", "w", encoding="utf-8") as file:
     for i in range(len(findings)):
         book = file.write(findings[i] + "\n")
 """
-
